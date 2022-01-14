124cc3b193ffd67a26ce9226944f372ec71ea09e
BW <wreford.b@etoncollege.org.uk>
2022-01-14 14:17:51 +0000

    removed empty stack

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -12,9 +12,6 @@ struct ContentView: View {
     let car = Car(make: "Mazda", model: "MX-5", topSpeed:125, acceleration: 7.7, handling: 5)
     
     var body: some View {
-        VStack{
-            
-        }
         Text("\(car.displayStats())")
             .padding()
     }
